1e596d83a5f121dd306f1b2971ede338ce0f5271
Abdul Waheed <86144678+captainWaheed@users.noreply.github.com>
2022-07-22 20:38:32 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express =require('express');
+// const express =require('express');
 
-const router = express();
+// const router = express();
 
-router.use("/CSS/style.css", express.static(__dirname + "/CSS/style.css"));
-router.use("/img", express.static(__dirname + "/img"));
+// router.use("/CSS/style.css", express.static(__dirname + "/CSS/style.css"));
+// router.use("/img", express.static(__dirname + "/img"));
 
 
-router.get("/", function(req,res){
-    res.sendFile(__dirname + "/index.html");
-})
+// router.get("/", function(req,res){
+//     res.sendFile(__dirname + "/index.html");
+// })
 
-router.listen(process.env.PORT || 5000,function(){
-    console.log("We are live");
-})
+// router.listen(process.env.PORT || 5000,function(){
+//     console.log("We are live");
+// })
